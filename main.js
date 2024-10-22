0a20466a287aca6a23ce11ad466eeca1cf02801e
Slay James <slayjms@yahoo.com>
2024-10-22 09:35:08 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,117 @@
+/******************************/
+//Your Code Here 
+const dayOfWeek = function(number){
+  switch(dayOfWeek){
+    case "1": {
+      console.log(Monday)
+      break
+    }
+    case "2": {
+      console.log(Tuesday)
+      break
+    }
+    case "3": {
+      console.log(Wednesday)
+      break
+    }
+    case "4": {
+      console.log(Thursday)
+      break
+    }
+    case "5": {
+      console.log(Friday)
+      break
+    }
+    case "6": {
+      console.log(Saturday)
+      break
+    }
+    case "7": {
+      console.log(Sunday)
+      break
+    }
+  }
+}
+
+
+const identifySeason = function(number){
+  switch(identifySeason){
+    case "1": {
+      console.log(Winter)
+      break
+    }
+    case "2": {
+      console.log(Winter)
+      break
+    }
+    case "3": {
+      console.log(Spring)
+      break
+    }
+    case "4": {
+      console.log(Spring)
+      break
+    }
+    case "5": {
+      console.log(Summer)
+      break
+    }
+    case "6": {
+      console.log(Summer)
+      break
+    }
+    case "7": {
+      console.log(Summer)
+      break
+    }
+    case "8": {
+      console.log(Autumn)
+      break
+    }
+    case "9": {
+      console.log(Autumn)
+      break
+    }
+    case "10": {
+      console.log(Autumn)
+      break
+    }
+    case "11": {
+      console.log(Winter)
+      break
+    }
+    case "12": {
+      console.log(Winter)
+      break
+    }
+  }
+}
+
+
+
+
+
+
+
+/****************************/
+// Our code here. Don't touch!
+if (typeof dayOfWeek === 'undefined') {
+    dayOfWeek = undefined
+  }
+  
+  if (typeof identifySeason === 'undefined') {
+    identifySeason = undefined
+  }
+  
+  if (typeof menuSelection === 'undefined') {
+    menuSelection = undefined
+  }
+
+  
+  
+  module.exports = {
+    dayOfWeek, 
+    identifySeason, 
+    menuSelection
+  }
+  
